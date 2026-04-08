fe5486c1dde917ccaddacd91e573051edcb2c84c
emanuelly-guline <emanuellygulineoliveira@gmail.com>
2026-04-08 20:22:49 -0300

    papagaio.c

---
+++ papagaio.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc, char *argv[])
+{
+if (argc == 1)
+{
+    printf("Você não digitou nada");
+    exit(0);
+}
+else
+{
+    printf("Você digitou %s", argv[1]);
+}
+printf("\nCriado por Emanuelly Guline Miguel de Oliveira");
+}
